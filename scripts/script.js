0cd2c8c5f9524e0409599223844549c2eaf4c400
Flo-Gaupp <101625129+Flo-Gaupp@users.noreply.github.com>
2022-04-12 09:06:32 +0200

    ES Lint

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* Main_Nav */
+
+document.querySelector(".top_nav .main_list_top_nav .tools_btn").addEventListener("click",
+    function() {
+        document.querySelector(".top_nav .main_list_top_nav .tools_btn").classList.toggle("show_menu");
+    }
+    , false);
+
