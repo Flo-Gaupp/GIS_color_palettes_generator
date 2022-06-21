c26a3aa56f09a8219aeca2c2e5716cc51cc63001
Flo-Gaupp <101625129+Flo-Gaupp@users.noreply.github.com>
2022-06-21 15:02:06 +0200

    moscow

--- scripts/server.js
+++ scripts/server.js
@@ -8,15 +8,23 @@ const hostname = "127.0.0.1"; // localhost
 const port = 3000;
 let paletteHtml = "";
 
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
-const mongoUrl = 'mongodb://localhost:27017'; // für lokale MongoDB
+const mongoUrl = "mongodb://localhost:27017"; // für lokale MongoDB
 const mongoClient = new mongodb.MongoClient(mongoUrl);
 const mongoPalettes = mongoClient.db("Color_Generator").collection("colorPalettes");
 const mongoUser = mongoClient.db("Color_Generator").collection("user");
 
 let signedIn = false;
 let currentUser;
+
+async function startServer() {
+    await mongoClient.connect();
+    server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    });
+}
+
 
 const server = http.createServer(async (request, response) => {
     response.statusCode = 200;
@@ -52,7 +60,6 @@ const server = http.createServer(async (request, response) => {
                 jsonString += data;
             });
             request.on("end", async () => {
-                await mongoClient.connect();
                 const userList = await mongoUser.find({ username: JSON.parse(jsonString).username }).toArray();
                 if (userList.length < 1) {
                     saveUser(jsonString);
@@ -73,7 +80,6 @@ const server = http.createServer(async (request, response) => {
                 jsonString += data;
             });
             request.on("end", async () => {
-                await mongoClient.connect();
                 const userList = await mongoUser.find({ username: JSON.parse(jsonString).username }).toArray();
                 if (userList.length < 1) {
                     response.end("user_error");
@@ -130,7 +136,6 @@ const server = http.createServer(async (request, response) => {
 
 // Save Palette to MongoDB
 async function savePalette(jsonString) {
-    await mongoClient.connect();
     await mongoPalettes.insertOne(JSON.parse(jsonString));
     currentUser.colorPalettes.push(JSON.parse(jsonString));
     await mongoUser.replaceOne({ username: currentUser.username }, currentUser);
@@ -138,18 +143,17 @@ async function savePalette(jsonString) {
 
 // Create User to MongoDB
 async function saveUser(jsonString) {
-    await mongoClient.connect();
     await mongoUser.insertOne(JSON.parse(jsonString));
 }
 
 // Create HTMl for LoadMore
 function paletteToHtml(allPalettes, counter) {
-    let paletteHtml = `<div class="colorbar">`;
+    let paletteHtml = "<div class=\"colorbar\">";
     for (let i = 0; i < allPalettes[counter].colors.length; i++) {
         const hexCode = allPalettes[counter].colors[i].hexCode;
         paletteHtml += `<div class="color" style="background-color:#${hexCode}"><span style="color:${getContrast50(hexCode)}">${hexCode}</span></div>`;
     }
-    paletteHtml += `</div>`;
+    paletteHtml += "</div>";
     return (paletteHtml);
 }
 
@@ -157,7 +161,4 @@ function getContrast50(hexcolor) {
     return (parseInt(hexcolor, 16) > 0xffffff / 2) ? "#000000" : "#ffffff";
 }
 
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+startServer();
